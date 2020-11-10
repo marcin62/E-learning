185753136b8ab0b6569345cf1f7968fa97ecce66
marcin62 <marcinkobus841@gmail.com>
2020-11-10 09:36:19 +0100

    New project

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+     <head>
+      <meta charset="utf-8">
+      <title>E Learning | Home</title>
+      <link rel="stylesheet" href="css/style.css"/>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"/>
+      </head>
+      <body>
+        <?php
+            include("inc/header.php");
+            include("inc/slider.php");
+            include("inc/home_cat.php");
+            include("inc/top_course.php");
+            include("inc/footer.php");
+        ?>
+      </body>
+</html>
